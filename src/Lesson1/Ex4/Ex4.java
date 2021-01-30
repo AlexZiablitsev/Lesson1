fd4a00e3b6cd32c9fd9a635cc5473199a754b0a4
Mirath7 <ziabliczev.lexa2010293111999@gmail.com>
2021-01-30 20:26:52 +0300

    Четвертое задание

---
+++ src/Lesson1/Ex4/Ex4.java
@@ -0,0 +1,13 @@
+package Lesson1.Ex4;
+
+public class Ex4 {
+    public static void main(String[] args) {
+
+        StringBuilder b = new StringBuilder("8965");
+
+       b = b.reverse(); // используем StringBuilder для переворота строки
+
+        System.out.println(b);
+
+    }
+}
