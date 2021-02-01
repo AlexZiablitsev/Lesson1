9651af5a37948592593a5f09b77eedb398e64e3b
Mirath7 <ziabliczev.lexa2010293111999@gmail.com>
2021-02-02 00:10:38 +0300

    add Exercise 4, добавил еще 1 вариан решения

--- src/Lesson1/Ex4/Ex4.java
+++ src/Lesson1/Ex4/Ex4.java
@@ -2,10 +2,20 @@ package Lesson1.Ex4;
 
 public class Ex4 {
     public static void main(String[] args) {
+        //Первый способ
+        int a = 9761;
+        int c = a/1000;
+        int d = a/100%10;
+        int e = a/10%10;
+        int f = a%10;
+        System.out.print(f);
+        System.out.print(e);
+        System.out.print(d);
+        System.out.println(c);
 
+        //Второй способ
         StringBuilder b = new StringBuilder("8965");
-
-       b = b.reverse(); // используем StringBuilder для переворота строки
+        b=b.reverse(); // используем StringBuilder для переворота строки
 
         System.out.println(b);
 
