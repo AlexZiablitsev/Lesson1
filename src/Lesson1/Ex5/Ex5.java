a643662d35b0c03353d425176b5103d431925d4a
Mirath7 <ziabliczev.lexa2010293111999@gmail.com>
2021-02-02 11:57:10 +0300

    Exercise 5, changed the second option

--- src/Lesson1/Ex5/Ex5.java
+++ src/Lesson1/Ex5/Ex5.java
@@ -6,28 +6,28 @@ public class Ex5 {
     public static void main(String[] args) {
         //Дано любое натуральное 4-х значное число. Верно ли, что все цифры числа
         //различны?
-       int num = 8571;
+        int num = 8578;
 
-       int a = num/1000;
-       int b = num/100 %10;
-       int c = num/10%10;
-       int d = num%10;
+        int a = num / 1000;
+        int b = num / 100 % 10;
+        int c = num / 10 % 10;
+        int d = num % 10;
 
-       if ((a!=b) && (a!=c) && (a!=d) && (b!=a) && (b!=c) && (b!=d)){
-           System.out.println("Верно");}
-       else {
-           out.println("Не верно");
-       }
-        //Второй вариант
-         if (a==b || a==c || a==d ){
-             out.println( "Не верно.");
-         }else if (b==a | b==c | b==d){
-             out.println("Не верно.");
-         }else{
-             out.println("Верно.");
-         }
-
+        if ((a != b) && (a != c) && (a != d) && (b != a) && (b != c) && (b != d)) {
+            System.out.println("Верно");
+        } else {
+            out.println("Не верно");
         }
+        //Второй вариант
+        if (a == b || a == c || a == d) {
+            out.println("Не верно.");
+        } else if (b == c || b == d) {
+            out.println("Не верно.");
+        } else {
+            out.println("Верно.");
+        }
+
+    }
 
 
 }
