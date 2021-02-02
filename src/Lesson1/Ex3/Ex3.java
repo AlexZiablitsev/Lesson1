177a96a98b7401508f3c4b9e726b7c8e28647842
Mirath7 <ziabliczev.lexa2010293111999@gmail.com>
2021-02-02 04:36:39 +0300

    Exercise 3,correction of spelling errors

--- src/Lesson1/Ex3/Ex3.java
+++ src/Lesson1/Ex3/Ex3.java
@@ -6,25 +6,25 @@ public class Ex3 {
 
         int sec = time % 60;
 
-        int m = (time-sec)/60;
+        int m = (time - sec) / 60;
 
-        int min = m%60;
+        int min = m % 60;
 
-        int h = (m -min)/60;
+        int h = (m - min) / 60;
 
-        int hour =h%24;
+        int hour = h % 24;
 
-        int d = (h-hour)/ 24;
+        int d = (h - hour) / 24;
 
-        int day = d%24;
+        int day = d % 24;
 
-        int w = (d- day)/7;
+        int w = (d - day) / 7;
 
-        int week = w%7;
+        int week = w % 7;
 
-        System.out.println("A: "+m+ " minute(s) " +sec+" second(s).");
-        System.out.println ("B: "+hour+" hour(s) " +min+ " minute(s) "+sec+" second(s).");
-        System.out.println("C: "+day+" day(s) "+hour+" hour(s) " +min+ " minute(s) "+sec+" second(s).");
-        System.out.println("D: "+week+" week(s) "+day+" day(s) "+hour+" hour(s) " + min + " minute(s) "+ sec+" second(s).");
+        System.out.println("A: " + m + " minute(s) " + sec + " second(s).");
+        System.out.println("B: " + hour + " hour(s) " + min + " minute(s) " + sec + " second(s).");
+        System.out.println("C: " + day + " day(s) " + hour + " hour(s) " + min + " minute(s) " + sec + " second(s).");
+        System.out.println("D: " + week + " week(s) " + day + " day(s) " + hour + " hour(s) " + min + " minute(s) " + sec + " second(s).");
     }
 }
