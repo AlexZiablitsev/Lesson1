90933f7d505b127afe9e15e41c85bdfc5996ae43
Mirath7 <ziabliczev.lexa2010293111999@gmail.com>
2021-02-02 11:59:56 +0300

    Exercise 7, correction of spelling errors

--- src/Lesson1/Ex7/Ex7.java
+++ src/Lesson1/Ex7/Ex7.java
@@ -5,12 +5,12 @@ public class Ex7 {
         /*меется прямоугольное отверстие размерами a и b, определить, можно ли его
         полностью закрыть круглой картонкой радиусом r.*/
         double sideA = 10, sideB = 7.5, radius = 3;
-        double diameter = 2*radius;
-        double hypotenuse = Math.sqrt((Math.pow(sideA, 2))+(Math.pow(sideB, 2)));
+        double diameter = 2 * radius;
+        double hypotenuse = Math.sqrt((Math.pow(sideA, 2)) + (Math.pow(sideB, 2)));
 
-        if (hypotenuse<=diameter){
+        if (hypotenuse <= diameter) {
             System.out.println("Картонка закроет отверстие");
-        }else {
+        } else {
             System.out.println("Картонка не закроет отверстие");
         }
     }
