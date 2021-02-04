83ea8fc3e97dad54a22a1d3180e30fdf8a68b796
Mirath7 <ziabliczev.lexa2010293111999@gmail.com>
2021-02-04 17:17:25 +0300

    Lesson 2, add exercise 2  is done.

--- src/Lesson2/Exercise2/Exercise2.java
+++ src/Lesson2/Exercise2/Exercise2.java
@@ -4,28 +4,27 @@ public class Exercise2 {
     public static void main(String[] args) {
         //Дано любое натуральное 4-х значное число. Верно ли, что все цифры числа
         //различны?
-        int num = 8578;
+        int num = 8571;
         int s = (int) Math.log10(num) + 1;
         int array[] = new int[s];
         for (int a = 0; a < array.length; a++) {
             array[a] = num % 10;
             num = num / 10;
         }
-        if (array[0] != array[1] && array[0] != array[2] && array[0]!= array[3]){
-            System.out.println("True");
-        }else {
-            System.out.println("False");
-        }
-
-            /*for (int a = 0; a < array.length; a++) {
-                for (int b = a + 1; b < array.length; b++) {
-
-                    if (array[a] != array[b]) {
-                        System.out.println("True.");
-                    } else {
-                        System.out.println("False.");// хз как сделать чтобы 1 раз выводило "True" или "False"
-                    }
+        for (int s1 = 0; s1 < array.length - 1; s1++) {
+            for (int i = 0; i < array.length - 1; i++) {
+                if (array[i] < array[i + 1]) {
+                    int mem = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = mem;
                 }
-            }*/
+            }
+        }
+        if (array[0] != array[1]) {
+            System.out.println("True");
+        } else {
+            System.out.println("False");
+
+        }
     }
 }
