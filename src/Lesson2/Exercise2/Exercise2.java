bdab51b3c1278dd882ea63c18ed7805dabc1d1ea
Mirath7 <ziabliczev.lexa2010293111999@gmail.com>
2021-02-05 15:29:57 +0300

    corrected the second exercise

--- src/Lesson2/Exercise2/Exercise2.java
+++ src/Lesson2/Exercise2/Exercise2.java
@@ -4,16 +4,19 @@ public class Exercise2 {
     public static void main(String[] args) {
         //Дано любое натуральное 4-х значное число. Верно ли, что все цифры числа
         //различны?
-        int num = 8571;
+        int num = 857155236;
         int s = (int) Math.log10(num) + 1;
         int array[] = new int[s];
+        int temp = num;
         for (int a = 0; a < array.length; a++) {
-            array[a] = num % 10;
-            num = num / 10;
+            array[a] = (int) (temp / Math.pow(10, s - 1));
+            temp = (int) (temp % Math.pow(10, s - 1));
+            s--;
         }
-
+        //First variant
         boolean flag = true;
-        out:for (int a = 0; a < array.length; a++) {
+        out:
+        for (int a = 0; a < array.length; a++) {
             for (int i = a + 1; i < array.length; i++) {
                 if (array[a] == array[i]) {
                     flag = false;
@@ -27,5 +30,22 @@ public class Exercise2 {
             System.out.println("False");
         }
 
+        // Second variant
+        int a = 0;
+        boolean flag1 = true;
+        while (a < array.length && flag1) {
+            for (int i = a + 1; i < array.length; i++) {
+                if (array[a] == array[i]) {
+                    flag1 = false;
+                    break;
+                }
+            }
+            a++;
+        }
+        if (flag1 == true) {
+            System.out.println("True");
+        } else {
+            System.out.println("False");
+        }
     }
 }
