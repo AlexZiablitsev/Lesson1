76f59050b52febb23535808e5b2d619a852a830f
Mirath7 <ziabliczev.lexa2010293111999@gmail.com>
2021-02-11 12:07:53 +0300

    Delete array Exercise3

--- src/Lesson3/Exercise3/Exercise3.java
+++ src/Lesson3/Exercise3/Exercise3.java
@@ -4,13 +4,9 @@ public class Exercise3 {
     public static void main(String[] args) {
         //Вычислить: 1+2+4+8+…+256
         int line = 256;
-        int array[] = new int[line];
-        for (int index = 0; index < array.length; index++) {
-            array[index] = index + 1;
-        }
         int sum = 0;
-        for (int a = 0; a < array.length; a++) {
-            sum = sum + array[a];
+        for (int a = 1; a <= line; a++) {
+            sum = sum + a;
         }
         System.out.println("Sum: " + sum);
     }
