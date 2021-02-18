5fe6618515367dd9bc90de6dc2c1fb7bda78ea18
Mirath7 <ziabliczev.lexa2010293111999@gmail.com>
2021-02-19 00:10:54 +0300

    corrected task3, task 4

--- src/Lesson3/Task3/Task3.java
+++ src/Lesson3/Task3/Task3.java
@@ -5,7 +5,7 @@ public class Task3 {
         //Вычислить: 1+2+4+8+…+256
         int line = 256;
         int sum = 0;
-        for (int a = 1; a <= line; a++) {
+        for (int a = 1; a <= line; a *= 2) {
             sum = sum + a;
         }
         System.out.println("Sum: " + sum);
--- src/Lesson3/Task4/Task4.java
+++ src/Lesson3/Task4/Task4.java
@@ -4,28 +4,17 @@ public class Task4 {
     public static void main(String[] args) {
         //Составьте программу, вычисляющую A*B, не пользуясь операцией
         //умножения.
-        int a = 2;
-        int b = -2;
+        int a = -3;
+        int b = 5;
         int moduleA = Math.abs(a);
         int moduleB = Math.abs(b);
-        // A & B > 0
+
         int result = 0;
-        if (a > 0 && b > 0 || moduleA > 0 & moduleB > 0) {
-            for (int i = 1; i <= b; i++) {
-                result = result + a;
-            }
+        for (int i = 1; i <= moduleB; i++) {
+            result = result + moduleA;
         }
-        // A > 0 & B < 0
-        if (a > 0 && b < 0) {
-            for (int i = -1; i >= b; i--) {
-                result = result - a;
-            }
-        }
-        // A & B < 0
-        if (a < 0 && b < 0) {
-            for (int i = -1; i >= b; i--) {
-                result = result - a;
-            }
+        if (a < 0 && b > 0 || a > 0 && b < 0) {
+            result = Math.negateExact(result);
         }
         System.out.println(result);
     }
