d01e3a8661f1c8256cd331e6d85d60c9f479d0cd
Mirath7 <ziabliczev.lexa2010293111999@gmail.com>
2021-02-24 18:57:22 +0300

    Lesson ,fix Task 1

--- src/Lesson6/Task1/List.java
+++ src/Lesson6/Task1/List.java
@@ -17,32 +17,32 @@ public class List implements MySuperArray {
     //Получить текущую длину массива
     @Override
     public int getLength() {
-        return array.length;
+        return this.array.length;
     }
 
     //Добавить элемент. Длинна массива должна увеличится на 1.
     @Override
     public Integer[] addElement(Integer element) {
-        Integer[] newArray = new Integer[getLength() + 1];
+        Integer[] newArray = new Integer[this.getLength() + 1];
         if (getLength() >= 0) System.arraycopy(array, 0, newArray, 0, getLength());
-        newArray[getLength()] = element;
-        array = newArray;
+        newArray[this.getLength()] = element;
+        this.array = newArray;
         return newArray;
     }
 
     //Получить элемент по индексу
     @Override
     public Integer getElement(int index) {
-        if (index > array.length) {
-            System.out.println("ВЫход за рамки массива, MAX_Index: " + (array.length - 1));
+        if (index > this.array.length) {
+            System.out.println("Выход за рамки массива, MAX_Index: " + (array.length - 1));
         }
-        return array[index];
+        return this.array[index];
     }
 
     //Удалить все текущие элементы (длина массива будет 0 псосле этой операции)
     @Override
     public void removeAllElements() {
-        array = new Integer[0];
+        this.array = new Integer[0];
 
     }
 
