3c2e08f008a888a65a1cb75b88cf6eea3a80a0ca
Mirath7 <ziabliczev.lexa2010293111999@gmail.com>
2021-02-24 19:07:14 +0300

    Lesson 5,fix Task 4

--- src/Lesson5/Task4/Fraction.java
+++ src/Lesson5/Task4/Fraction.java
@@ -6,69 +6,69 @@ public class Fraction {
     //Объявить и инициализировать массив из k дробей, ввести/вывести значения для массива дробей. Создать массив объектов
     // и передать его в метод, который изменяет каждый элемент массива с четным
     //индексом путем добавления следующего за ним элемента.
-    private int chislitel;
-    private int znamenatel;
+    private int numerator;
+    private int denominator;
 
 
-    public Fraction(int chislitel, int znamenatel) {
-        this.chislitel = chislitel;
-        this.znamenatel = znamenatel;
+    public Fraction(int numerator, int znamenatel) {
+        this.numerator = numerator;
+        this.denominator = znamenatel;
         if (znamenatel == 0) {
             throw new IllegalArgumentException("Знаменатель не должен ровняться нулю.");
         }
     }
 
     public int getChislitel() {
-        return chislitel;
+        return numerator;
     }
 
     public int getZnamenatel() {
-        return znamenatel;
+        return denominator;
     }
 
     public void print() {
-        System.out.println(String.format("%s/%s", chislitel, znamenatel));
+        System.out.println(String.format("%s/%s", numerator, denominator));
     }
 
     protected Fraction addition(Fraction b) {
-        chislitel = chislitel * b.znamenatel + b.chislitel * znamenatel;
-        znamenatel = znamenatel * b.znamenatel;
+        numerator = numerator * b.denominator + b.numerator * denominator;
+        denominator = denominator * b.denominator;
         Fraction c = format();
         return c;
     }
 
     protected Fraction subtraction(Fraction b) {
-        chislitel = chislitel * b.znamenatel - b.chislitel * znamenatel;
-        znamenatel = znamenatel * b.znamenatel;
+        numerator = numerator * b.denominator - b.numerator * denominator;
+        denominator = denominator * b.denominator;
         Fraction c = format();
         return c;
     }
 
     protected Fraction multiplication(Fraction b) {
-        chislitel = chislitel * b.chislitel;
-        znamenatel = znamenatel * b.znamenatel;
+        numerator = numerator * b.numerator;
+        denominator = denominator * b.denominator;
         Fraction c = format();
         return c;
     }
 
     protected Fraction division(Fraction b) {
-        chislitel = chislitel * b.znamenatel;
-        znamenatel = znamenatel * b.chislitel;
+        numerator = numerator * b.denominator;
+        denominator = denominator * b.numerator;
         Fraction c = format();
         return c;
     }
 
     protected Fraction format() {
-        int min = Math.min(chislitel, znamenatel);
+        int min = Math.min(numerator, denominator);
         Fraction fraction = new Fraction(getChislitel(), getZnamenatel());
         for (int i = min; i > 1; i--) {
-            if ((fraction.chislitel % i == 0) && (fraction.znamenatel % i == 0)) {
+            if ((fraction.numerator % i == 0) && (fraction.denominator % i == 0)) {
                 Fraction fraction1 = fraction;
-                fraction1.chislitel = fraction.chislitel / i;
-                fraction1.znamenatel = fraction.znamenatel / i;
+                fraction1.numerator = fraction.numerator / i;
+                fraction1.denominator = fraction.denominator / i;
                 fraction = fraction1;
-                chislitel = fraction1.chislitel;
-                znamenatel = fraction1.znamenatel;
+                numerator = fraction1.numerator;
+                denominator = fraction1.denominator;
             }
         }
         return fraction;
